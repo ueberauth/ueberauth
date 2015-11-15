a2b44f264650960de5a5b5b4620c11e57e5e2fff
Sonny Scroggin <scrogson@gmail.com>
2015-11-15 12:55:53 -0600

    Prep for a hex and hexdocs release

--- mix.exs
+++ mix.exs
@@ -1,34 +1,41 @@
 defmodule Ueberauth.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :ueberauth,
-     version: "0.0.1",
+     name: "Überauth",
+     version: @version,
      elixir: "~> 1.1",
      package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     source_url: "https://github.com/scrogson/ueberauth",
+     homepage_url: "https://github.com/scrogson/ueberauth",
+     description: description,
+     deps: deps,
+     docs: docs]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:plug, "~>1.0"}]
+    [{:plug, "~>1.0"},
+
+     # Docs dependencies
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.10", only: :dev}]
+  end
+
+  defp docs do
+    [main: "Ueberauth"]
+  end
+
+  defp description do
+    "An Elixir Authentication System for Plug-based Web Applications"
   end
 
   defp package do
