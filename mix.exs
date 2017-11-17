d648d8f3938f3e64923d381e243f887ed75bf427
Sean Callan <seancallan@gmail.com>
2017-11-17 12:19:18 -0700

    Release 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
 defmodule Ueberauth.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
 
   def project do
     [app: :ueberauth,
      name: "Überauth",
      version: @version,
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
