954cd7aecf7ab31faec7bed56d379bf7593708f1
Jon Anderson <jon.david.anderson@gmail.com>
2018-12-28 16:24:24 -0800

    Added exception for missing configuration.
    When building, if Ueberauth configuration isn't present in the
    application evironment an exception is now raised.

--- lib/ueberauth.ex
+++ lib/ueberauth.ex
@@ -193,8 +193,13 @@ defmodule Ueberauth do
 
   @doc false
   def init(opts \\ []) do
+    config_opts = Application.get_env(:ueberauth, Ueberauth)
+    if config_opts == nil do
+      raise "Missing configuration for this environment."
+    end
+
     {provider_list, opts} = Keyword.pop(opts, :providers, :all)
-    opts = Keyword.merge(Application.get_env(:ueberauth, Ueberauth), opts)
+    opts = Keyword.merge(config_opts, opts)
 
     {base_path, opts} = Keyword.pop(opts, :base_path, "/auth")
     {all_providers, _opts} = Keyword.pop(opts, :providers)
