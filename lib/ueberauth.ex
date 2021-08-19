2872119a997895603a9ef6b79f1c0daf9e1cff21
Gabriel Giordano <hello@gabrielgiordano.com>
2021-08-19 15:56:00 -0300

    Add custom URL resolution scheme option (#144)
    * Add request_scheme and callback_scheme options
    
    * Add callback_scheme option test
    
    * Add scheme for callback and request url functions
    
    * Bump minor version
    
    * Add custom scheme docs

--- lib/ueberauth.ex
+++ lib/ueberauth.ex
@@ -169,6 +169,22 @@ defmodule Ueberauth do
         ],
         json_library: Poison # or Jason
 
+  #### Customizing Schemes
+
+  By default, Ueberauth uses your `Plug.Conn` scheme.
+
+  A custom scheme can be provided through the request header `X-Forwarded-Proto`.
+
+  For example, in Nginx you can set it.
+
+      proxy_set_header X-Forwarded-Proto $scheme;
+
+  Another option is to override this via options to your strategy.
+
+      providers: [
+        identity: {Ueberauth.Strategies.Identity, [callback_scheme: "https"]}
+      ]
+
   #### Http Methods
 
   By default, all callback URLs are only available via the `"GET"` method. You
@@ -407,7 +423,9 @@ defmodule Ueberauth do
     %{
       strategy: module,
       strategy_name: name,
+      request_scheme: Keyword.get(options, :request_scheme),
       request_path: get_request_path(base_path, strategy),
+      callback_scheme: Keyword.get(options, :callback_scheme),
       callback_path: get_callback_path(base_path, strategy),
       callback_methods: get_callback_methods(options),
       options: options,
